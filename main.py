e53777d9a15aa170f4e87af7147bd5c00b3c28c4
Mikhail Palvov <mpavlov@megaputer.ru>
2025-08-26 09:35:04 +0300

    SQLite database added to get a random film

--- main.py
+++ main.py
@@ -1,22 +1,45 @@
 import random
+import sqlite3
 
+from data import Answers, Bot, Chat, Movies, User
 from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters
-
-from data import Answers, Bot, Chat, Films, Movies, User
+from telegram.ext import (
+    ApplicationBuilder,
+    CommandHandler,
+    ContextTypes,
+    MessageHandler,
+    filters,
+)
 
 ANSWERS = Answers()
 BOT = Bot()
 CHAT = Chat()
-FILMS = Films()
 MOVIES = Movies()
 USER = User()
 
-def find_random_film(films: tuple[str]) -> str:
 
-    """The function accepts a list with a list of films and returns a random film."""
+def find_random_film() -> str:
 
-    return random.choice(films)
+    """The function establishes the database connection to get a random film."""
+
+    try:
+        connection = sqlite3.connect("films.db")
+        cursor = connection.cursor()
+
+        cursor.execute("SELECT FILM FROM films ORDER BY RANDOM() LIMIT 1")
+        random_film = cursor.fetchone()[0]
+
+        connection.commit()
+        connection.close()
+
+        return random_film
+
+    except Exception:
+        print("Connection is not established")
+        connection.close()
+
+        return None
+
 
 def give_random_answer(answers: tuple[str]) -> str:
 
@@ -24,25 +47,30 @@ def give_random_answer(answers: tuple[str]) -> str:
 
     return random.choice(answers)
 
-async def get_movie(update:Update, context:ContextTypes.DEFAULT_TYPE):
+
+async def get_movie(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     """The function returns a film from the current list (for internal use only)."""
 
     if not update.message:
         return None
 
-    await update.message.reply_text(find_random_film(MOVIES.GET_ONE_FROM_A_LIST_OF_MOVIES))
+    await update.message.reply_text(
+        find_random_film(MOVIES.GET_ONE_FROM_A_LIST_OF_MOVIES)
+    )
 
-async def get_film(update:Update, context:ContextTypes.DEFAULT_TYPE):
+
+async def get_film(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     """The function returns a film from the main list of films."""
 
     if not update.message:
         return None
 
-    await update.message.reply_text(find_random_film(FILMS.GET_ONE_FROM_A_LIST_OF_FILMS))
+    await update.message.reply_text(find_random_film())
 
-async def answer_to_specific_user(update:Update, context:ContextTypes.DEFAULT_TYPE):
+
+async def answer_to_specific_user(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     """The function handles a user's message only from a specific user."""
 
@@ -50,24 +78,42 @@ async def answer_to_specific_user(update:Update, context:ContextTypes.DEFAULT_TY
         return None
 
     if update.message.from_user.id == USER.GET_USER_ID:
-        await update.message.reply_text(give_random_answer(ANSWERS.GET_ONE_FROM_A_LIST_OF_ANSWERS), do_quote=True, disable_notification=True)
+        await update.message.reply_text(
+            give_random_answer(ANSWERS.GET_ONE_FROM_A_LIST_OF_ANSWERS),
+            do_quote=True,
+            disable_notification=True,
+        )
 
-async def get_any(update:Update, context:ContextTypes.DEFAULT_TYPE):
+
+async def get_any(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     """The function handles any other command sent, i.e. a command which is not registered as a handler."""
 
-    await update.message.reply_text("Используйте команду /film, чтобы получить случайный фильм из списка.")
+    await update.message.reply_text(
+        "Используйте команду /film, чтобы получить случайный фильм из списка."
+    )
+
 
 def main():
 
     app = ApplicationBuilder().token(BOT.GET_BOT_TOKEN).build()
 
-    app.add_handler(CommandHandler('movie', get_movie))
-    app.add_handler(CommandHandler('film', get_film))
-    app.add_handler(MessageHandler((filters.Chat(chat_id=CHAT.GET_CHAT_ID) | filters.User(user_id=USER.GET_USER_ID)) & filters.TEXT, answer_to_specific_user))
+    app.add_handler(CommandHandler("movie", get_movie))
+    app.add_handler(CommandHandler("film", get_film))
+    app.add_handler(
+        MessageHandler(
+            (
+                filters.Chat(chat_id=CHAT.GET_CHAT_ID)
+                | filters.User(user_id=USER.GET_USER_ID)
+            )
+            & filters.TEXT,
+            answer_to_specific_user,
+        )
+    )
     app.add_handler(MessageHandler(filters.COMMAND, get_any))
 
     app.run_polling()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
